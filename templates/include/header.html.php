4e6cf2bea22bdcbcb13dfc99a326ef6b3fefdd6d
PXgamer <owzie123@gmail.com>
2017-07-10 15:01:31 +0100

    Removed images directory from public/

--- templates/include/header.html.php
+++ templates/include/header.html.php
@@ -1,8 +1,12 @@
 <?php
 use pxgamer\TorrentTrader\Template;
+
 ?>
 <head>
     <title><?= Template\Meta::getTitle() ?></title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+    <meta name="theme-color" content="#ffffff">
+    <script src="/assets/<?= $_ENV['DEFAULT_THEME'] ?>/js/application.min.js"></script>
+    <link rel="stylesheet" href="/assets/<?= $_ENV['DEFAULT_THEME'] ?>/css/styles.min.css">
 </head>
