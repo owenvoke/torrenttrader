b20352a5a554dc074a4f7801b2fbee14b74b647f
PXgamer <owzie123@gmail.com>
2017-07-10 15:52:15 +0100

    Changed 'application.min.js' to 'app.js' and 'styles.min.css' to 'app.css'

--- templates/include/header.html.php
+++ templates/include/header.html.php
@@ -7,6 +7,6 @@ use pxgamer\TorrentTrader\Template;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
     <meta name="theme-color" content="#ffffff">
-    <script src="/assets/js/application.min.js"></script>
-    <link rel="stylesheet" href="/assets/css/styles.min.css">
+    <script src="/assets/js/app.js"></script>
+    <link rel="stylesheet" href="/assets/css/app.css">
 </head>
