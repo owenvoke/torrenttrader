caff97155a8a6a74c0129e7952b1ae2766fe4662
PXgamer <owzie123@gmail.com>
2017-07-11 11:27:25 +0100

    Redone the Template\Output class

--- src/Template/Output.php
+++ src/Template/Output.php
@@ -4,21 +4,88 @@ namespace pxgamer\TorrentTrader\Template;
 
 class Output
 {
-    private static $content;
+    const HTTP_OK = 200;
 
-    /**
-     * @return string
-     */
-    public static function getContent()
+    private $m_aHeaders;
+    private $m_sBody;
+    private $m_sStatus;
+    private $m_sContentType;
+    private $m_aViewVariables;
+
+    public function __construct()
     {
-        return self::$content;
+        $this->m_aHeaders = array();
+        $this->m_sBody = '';
+        $this->m_sStatus = self::HTTP_OK;
+        $this->m_sContentType = 'text/html; charset=UTF-8';
+        $this->m_aViewVariables = array();
     }
 
-    /**
-     * @param string $content
-     */
-    public static function setContent(string $content)
+    public function renderTemplate($p_aOptions)
     {
-        self::$content = $content;
+        extract($this->m_aViewVariables);
+
+        if (isset($p_aOptions['template']) && $p_aOptions['template']) {
+            $sContent = $p_aOptions['file'];
+            $this->checkTemplateExists($sContent);
+            $sFile = '../templates/template.html.php';
+        } else {
+            $sFile = $p_aOptions['file'];
+        }
+
+        $this->checkTemplateExists($sFile);
+
+        ob_start();
+        /** @noinspection PhpIncludeInspection */
+        include($sFile);
+        $this->m_sBody = ob_get_clean();
+    }
+
+    public function renderText($p_aOptions)
+    {
+        $this->m_sBody = (string)$p_aOptions;
+    }
+
+    public function send()
+    {
+        //set status
+        if (!is_null($this->m_sStatus)) {
+            header($this->m_sStatus);
+        }
+        //set headers
+        foreach ($this->m_aHeaders as $sKey => $sValue) {
+            header("$sKey: $sValue");
+        }
+        //set content type
+        header("Content-type: " . $this->m_sContentType);
+        //output body
+        echo $this->m_sBody;
+    }
+
+    public function setContentType($p_sContentType)
+    {
+        $this->m_sContentType = $p_sContentType;
+    }
+
+    public function setHeader($p_sKey, $p_sValue)
+    {
+        $this->m_aHeaders[$p_sKey] = $p_sValue;
+    }
+
+    public function setStatus($p_sStatusCode)
+    {
+        $this->m_sStatus = $p_sStatusCode;
+    }
+
+    public function setViewVariable($p_sKey, $p_sValue)
+    {
+        $this->m_aViewVariables[$p_sKey] = $p_sValue;
+    }
+
+    private function checkTemplateExists($p_sTemplate)
+    {
+        if (!file_exists($p_sTemplate)) {
+            throw new \Exception('Template not found.');
+        }
     }
 }
