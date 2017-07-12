57e653b93e0ef212e226654b28a72904e78b12e8
PXgamer <owzie123@gmail.com>
2017-07-12 14:50:54 +0100

    Added default site_notice template

---
+++ templates/block_modules/site_notice.html.php
@@ -0,0 +1,3 @@
+<strong>Welcome to TorrentTrader 7</strong>
+<p>This is currently still in development, please report any bugs to the
+    <a href="//github.com/PXgamer/TorrentTrader/issues" target="_blank">TorrentTrader7 issue tracker</a>.</p>
