49bf63c601fab8e55a858171ab354c12dac0a3d5
PXgamer <owzie123@gmail.com>
2017-07-12 15:20:33 +0100

    Added ID div to the site_notice template

--- templates/block_modules/site_notice.html.php
+++ templates/block_modules/site_notice.html.php
@@ -1,3 +1,5 @@
-<strong>Welcome to TorrentTrader 7</strong>
-<p>This is currently still in development, please report any bugs to the
-    <a href="//github.com/PXgamer/TorrentTrader/issues" target="_blank">TorrentTrader7 issue tracker</a>.</p>
+<div id="site_notice">
+    <strong>Welcome to TorrentTrader 7</strong>
+    <p>This is currently still in development, please report any bugs to the
+        <a href="//github.com/PXgamer/TorrentTrader/issues" target="_blank">TorrentTrader7 issue tracker</a>.</p>
+</div>
