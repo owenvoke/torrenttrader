9104360142d639d12128ea20334ba6d7eb912a40
PXgamer <owzie123@gmail.com>
2017-07-12 15:21:55 +0100

    Begin adding the 'Latest Torrents' block

--- src/Welcome/Controller.php
+++ src/Welcome/Controller.php
@@ -8,6 +8,16 @@ class Controller extends BaseController
 {
     public function index()
     {
+        if ($_ENV['MEMBERS_ONLY']) {
+            // Select latest torrents
+            $stmt = $this->database->query("SELECT torrents.id, torrents.anon, torrents.announce, torrents.category, torrents.leechers, torrents.nfo, torrents.seeders, torrents.name, torrents.times_completed, torrents.size, torrents.added, torrents.comments, torrents.numfiles, torrents.filename, torrents.owner, torrents.external, torrents.freeleech, categories.name AS cat_name, categories.image AS cat_pic, categories.parent_cat AS cat_parent, users.username, users.privacy, IF(torrents.numratings < 2, NULL, ROUND(torrents.ratingsum / torrents.numratings, 1)) AS rating FROM torrents LEFT JOIN categories ON category = categories.id LEFT JOIN users ON torrents.owner = users.id WHERE visible = 'yes' AND banned = 'no' ORDER BY id DESC LIMIT 25");
+
+            if ($stmt->execute()) {
+                $aIndexTorrentsList = $stmt->fetchAll();
+                $this->output->setViewVariable('aIndexTorrentsList', $aIndexTorrentsList);
+            }
+        }
+
         $this->output->renderTemplate([
             'file' => ROOT_PATH . 'templates/welcome/index.html.php',
             'template' => true
--- templates/welcome/index.html.php
+++ templates/welcome/index.html.php
@@ -1,16 +1,20 @@
 <?php
 /**
+ * @var array $aIndexTorrentsList
  */
 ?>
 <div class="container-fluid">
     <?php if ($_ENV['SITE_NOTICE']) { ?>
-        <div id="site_notice">
-            <?php include __DIR__ . '/../block_modules/site_notice.html.php'; ?>
-        </div>
+        <?php include __DIR__ . '/../block_modules/site_notice.html.php'; ?>
     <?php } ?>
+
     <?php if ($_ENV['SHOUT_BOX']) { ?>
         <div id="shout_box_frame">
             <?php //TODO: Add Shoutbox ?>
         </div>
+    <?php } ?>
+
+    <?php if ($_ENV['MEMBERS_ONLY']) { ?>
+        <?php include __DIR__ . '/../block_modules/index_torrents.html.php'; ?>
     <?php } ?>
 </div>
