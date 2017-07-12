6d90554aa496fbe6a8deee8b59fef85db0e62012
PXgamer <owzie123@gmail.com>
2017-07-12 14:57:50 +0100

    Added PDO connection to the BaseController class

--- src/Configuration/BaseController.php
+++ src/Configuration/BaseController.php
@@ -6,8 +6,12 @@ use pxgamer\TorrentTrader\Template\Output;
 
 class BaseController
 {
+    protected $output;
+    protected $database;
+
     public function __construct()
     {
         $this->output = new Output();
+        $this->database = new \PDO($_ENV['DB_DSN'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
     }
 }
