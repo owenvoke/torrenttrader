a9fc869ded4336343afc48f1bac1ce369b4ab139
PXgamer <owzie123@gmail.com>
2017-09-13 12:38:37 +0100

    Added Model, Controller and Factory for Torrents

---
+++ app/Http/Controllers/TorrentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Torrent;
+use Illuminate\Http\Request;
+
+class TorrentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Torrent  $torrent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Torrent $torrent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Torrent  $torrent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Torrent $torrent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Torrent  $torrent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Torrent $torrent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Torrent  $torrent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Torrent $torrent)
+    {
+        //
+    }
+}
---
+++ database/factories/TorrentFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Torrent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
