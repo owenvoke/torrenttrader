37c242c0288a6bd216c16377d52815eb4ed0049e
PXgamer <owzie123@gmail.com>
2017-09-14 09:21:38 +0100

    Added create_torrents_table migration

---
+++ database/migrations/2017_09_14_082032_create_torrents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTorrentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('torrents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('info_hash', 40);
+            $table->string('name', 500);
+            $table->text('description');
+            $table->bigInteger('category');
+            $table->bigInteger('size');
+            $table->dateTime('added');
+            $table->bigInteger('downloads');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('torrents');
+    }
+}
