2d618e469f07ab891a245f59d9ff095c00455876
PXgamer <owzie123@gmail.com>
2017-09-14 23:30:36 +0100

    Updated TorrentFactory

--- database/factories/TorrentFactory.php
+++ database/factories/TorrentFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Torrent::class, function (Faker $faker) {
     return [
-        //
+        'info_hash' => sha1(mt_rand()),
+        'name' => str_random(20)
     ];
 });
