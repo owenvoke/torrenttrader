804d5662051fa91d3111526115f984cdd72c7df6
PXgamer <owzie123@gmail.com>
2017-09-14 23:09:45 +0100

    Added `users.acl` column

--- database/migrations/2017_09_14_081741_create_users_table.php
+++ database/migrations/2017_09_14_081741_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('acl');
 
             $table->rememberToken();
             $table->timestamps();
