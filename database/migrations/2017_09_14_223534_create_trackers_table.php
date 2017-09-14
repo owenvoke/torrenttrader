cc50fbf581ceaaa942d8ca202f9a559b78886c5d
PXgamer <owzie123@gmail.com>
2017-09-14 23:38:39 +0100

    Added Tracker model, factory and create_trackers_table migration

---
+++ database/migrations/2017_09_14_223534_create_trackers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrackersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url', 500);
+            $table->boolean('status')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trackers');
+    }
+}
