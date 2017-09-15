7dd8bfde76dbf7bfe3ea3933800fa552866390ba
PXgamer <owzie123@gmail.com>
2017-09-15 16:10:54 +0100

    Updated _id columns for FaqFactory and FileFactory

--- database/factories/FaqFactory.php
+++ database/factories/FaqFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\Faq::class, function (Faker $faker) {
     return [
-        'category' => function () {
+        'category_id' => function () {
             return factory(App\FaqCategory::class)->create()->id;
         },
         'question' => $faker->realText(),
--- database/factories/FileFactory.php
+++ database/factories/FileFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\File::class, function (Faker $faker) {
     return [
-        'torrent' => function () {
+        'torrent_id' => function () {
             return factory(\App\Torrent::class)->create()->id;
         },
         'path' => $faker->file(),
