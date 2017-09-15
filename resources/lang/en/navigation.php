86c0e4e89621859acf5357a4d62dbf0417fc6a42
PXgamer <owzie123@gmail.com>
2017-09-16 00:31:33 +0100

    Added additional navigation lang

--- resources/lang/en/navigation.php
+++ resources/lang/en/navigation.php
@@ -8,6 +8,10 @@ return [
     |--------------------------------------------------------------------------    |
     */
 
+    'search'          => 'Search',
+    'sign_up'         => 'Sign Up',
+    'log_in'          => 'Log In',
+    'logout'          => 'Logout',
     'torrents'        => 'Torrents',
     'browse_torrents' => 'Browse',
 
