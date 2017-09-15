7c19248136da6da60075563362c4413c3d6b6956
PXgamer <owzie123@gmail.com>
2017-09-15 16:10:24 +0100

    Added hasMany() methods to Torrent model

--- app/Torrent.php
+++ app/Torrent.php
@@ -4,7 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Torrent
+ * @package App
+ */
 class Torrent extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function files()
+    {
+        return $this->hasMany('App\File');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function trackers()
+    {
+        return $this->hasMany('App\TorrentTracker');
+    }
 }
