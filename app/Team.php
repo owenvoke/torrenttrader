3916bd98f26e962b21ebb3a21f48b8d0cae049e7
PXgamer <owzie123@gmail.com>
2017-09-15 22:02:44 +0100

    Added Team->users() relationship

--- app/Team.php
+++ app/Team.php
@@ -4,7 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Team
+ * @package App
+ */
 class Team extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function users()
+    {
+        return $this->hasMany('App\User');
+    }
 }
