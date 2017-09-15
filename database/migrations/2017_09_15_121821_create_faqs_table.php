70cebed9bc1c25bb37de00fdce79e93287b4f571
PXgamer <owzie123@gmail.com>
2017-09-15 13:37:19 +0100

    Added Faq model, factory and migration

---
+++ database/migrations/2017_09_15_121821_create_faqs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFaqsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faqs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category');
+            $table->string('question', 500);
+            $table->text('answer');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faqs');
+    }
+}
