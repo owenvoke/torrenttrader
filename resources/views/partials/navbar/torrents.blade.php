222b7c8869f19bd3144d0af73ed6675004dae502
PXgamer <owzie123@gmail.com>
2017-09-16 00:23:13 +0100

    Updated partials.navbar.torrents to use localised text

--- resources/views/partials/navbar/torrents.blade.php
+++ resources/views/partials/navbar/torrents.blade.php
@@ -1,9 +1,10 @@
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
-        Torrents
+        <span>@lang('navigation.torrents')</span>
     </a>
     <div class="dropdown-menu">
-        <a class="dropdown-item"
-           href="{{ route('torrents.index') }}">Browse Torrents</a>
+        <a class="dropdown-item" href="{{ route('torrents.index') }}">
+            <span>@lang('navigation.browse_torrents')</span>
+        </a>
     </div>
 </li>
