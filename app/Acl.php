3b729758e46c8ceeda8e4d0cb83b345c2131bc4b
PXgamer <owzie123@gmail.com>
2017-09-15 22:09:47 +0100

    Added users() method to App\Acl

--- app/Acl.php
+++ app/Acl.php
@@ -10,5 +10,11 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Acl extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
