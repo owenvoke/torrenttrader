4e9eea2868a8a385d04193faf2abf74cfe2bc001
PXgamer <owzie123@gmail.com>
2017-09-15 23:11:26 +0100

    Updated partials.navbar.torrents view to BS4

--- resources/views/partials/navbar/torrents.blade.php
+++ resources/views/partials/navbar/torrents.blade.php
@@ -1,10 +1,9 @@
-<li class="dropdown">
-    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Torrents</a>
-    <ul class="dropdown-menu">
-        <li>
-            <a class="dropdown-item" href="{{ route('torrents.index') }}">
-                <span>Browse Torrents</span>
-            </a>
-        </li>
-    </ul>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+        Torrents
+    </a>
+    <div class="dropdown-menu">
+        <a class="dropdown-item"
+           href="{{ route('torrents.index') }}">Browse Torrents</a>
+    </div>
 </li>
