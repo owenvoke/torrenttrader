a98255005ff0486a873cd06ca132f1367c1e969f
PXgamer <owzie123@gmail.com>
2017-09-15 13:50:34 +0100

    Added Team model, factory and migration

---
+++ database/migrations/2017_09_15_124519_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique();
+            $table->unsignedInteger('owner');
+            $table->string('description', 500)->default('');
+            $table->string('logo')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
