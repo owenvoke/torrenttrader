e4995f2b461dc3fe37762c96a7c43fca570380cc
PXgamer <owzie123@gmail.com>
2017-09-16 15:49:01 +0100

    Getting the hang of these relationship links for pivot tables

--- app/TorrentTracker.php
+++ app/TorrentTracker.php
@@ -10,5 +10,24 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
  */
 class TorrentTracker extends Pivot
 {
+    /**
+     * @var string
+     */
     protected $table = 'torrent_trackers';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function tracker()
+    {
+        return $this->belongsTo('App\Tracker');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function torrent()
+    {
+        return $this->belongsTo('App\Torrent');
+    }
 }
--- app/Tracker.php
+++ app/Tracker.php
@@ -10,5 +10,11 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Tracker extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function torrents()
+    {
+        return $this->hasMany('App\TorrentTracker');
+    }
 }
