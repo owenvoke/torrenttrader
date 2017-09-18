0b809b696157a7a6dd71ea4f96004a7ec2300798
PXgamer <owzie123@gmail.com>
2017-09-18 15:14:22 +0100

    Added TorrentTagFactory

---
+++ database/factories/TorrentTagFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\TorrentTag::class, function (Faker $faker) {
+    return [
+        'torrent_id' => function () {
+            return factory(App\Torrent::class)->create()->id;
+        },
+        'tag_id' => function () {
+            return factory(App\Tag::class)->create()->id;
+        }
+    ];
+});
