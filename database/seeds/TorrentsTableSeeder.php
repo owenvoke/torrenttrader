bbcc8ee8423ace62ed5ab66c77f8ec9d019e751f
PXgamer <owzie123@gmail.com>
2017-09-18 17:34:49 +0100

    Added TorrentsTableSeeder

--- database/seeds/TorrentsTableSeeder.php
+++ database/seeds/TorrentsTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class TorrentsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(TorrentsTableSeeder::class);
+        factory(App\Torrent::class, 5)->create();
     }
 }
