603ee01b032287ab1885f7d2a0cec540520df92a
PXgamer <owzie123@gmail.com>
2017-09-18 15:02:14 +0100

    Added UserTeam pivot, migration and factory

---
+++ app/UserTeam.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+/**
+ * Class UserTeam
+ * @package App
+ */
+class UserTeam extends Pivot
+{
+    /**
+     * @var string
+     */
+    protected $table = 'user_teams';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function team()
+    {
+        return $this->belongsTo('App\Team');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
---
+++ database/factories/UserTeamFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\UserTeam::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'team_id' => function () {
+            return factory(App\Team::class)->create()->id;
+        }
+    ];
+});
