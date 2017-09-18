12d3a2d2197d21313649d39dd1eda97dd949a406
PXgamer <owzie123@gmail.com>
2017-09-18 15:11:14 +0100

    Added category_id and user_id to TorrentFactory

--- database/factories/TorrentFactory.php
+++ database/factories/TorrentFactory.php
@@ -6,6 +6,12 @@ use Faker\Generator as Faker;
 $factory->define(App\Torrent::class, function (Faker $faker) {
     return [
         'info_hash' => sha1(mt_rand()),
-        'title' => $faker->realText()
+        'title' => $faker->realText(),
+        'category_id' => function () {
+            return factory(App\Category::class)->create()->id;
+        },
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        }
     ];
 });
