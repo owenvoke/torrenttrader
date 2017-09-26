fe4883c72ad5e42d5a8124683c7fe20bc51174cd
PXgamer <owzie123@gmail.com>
2017-09-26 10:02:54 +0100

    Removed `remember_token` from UserFactory as this shouldn't be generated.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,7 +10,6 @@ $factory->define(App\User::class, function (Faker $faker) {
         'username' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
         'acl_id' => function () {
             return factory(App\Acl::class)->create()->id;
         },
